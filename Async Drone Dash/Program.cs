f59feabb37e77c0262333fbf18e209f835ed8f00
stian126jg-svg <stian1.26jg@gmail.com>
2026-08-10 11:20:59 +0200

    Quick test of our DroneModel. Seems to be working fine!

--- Async Drone Dash/Program.cs
+++ Async Drone Dash/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AsyncDroneDash.Models;
+
+DroneModel drone = new DroneModel("Falcon-1", 5, 500);
+
+Console.WriteLine($"Drone: {drone.Name}");
+Console.WriteLine($"Checkpoints: {drone.MaxCheckpoints}");
+Console.WriteLine($"Delay: {drone.DelayMs} ms");
